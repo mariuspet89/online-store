d7737f8082a69f19bb0944b8116efed997914773
marius.petrut <marius.petrut@accesa.eu>
2021-02-24 13:09:10 +0200

    entities+dtos

---
+++ src/main/java/eu/accesa/onlinestore/model/dto/UserDto.java
@@ -0,0 +1,88 @@
+package eu.accesa.onlinestore.model.dto;
+
+import eu.accesa.onlinestore.model.entity.Address;
+
+public class UserDto {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String telephone;
+    private String sex;
+    private String password;
+    private Address address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
