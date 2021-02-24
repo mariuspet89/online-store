0bbe054e28d21b213523003d34a27a3d0f19d73d
Dan Goia <dan.goia@accesa.eu>
2021-02-24 14:39:28 +0200

    updated user collection name

--- src/main/java/eu/accesa/onlinestore/model/entity/UserEntity.java
+++ src/main/java/eu/accesa/onlinestore/model/entity/UserEntity.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
-@Document(collection = "user")
+@Document(collection = "users")
 public class UserEntity {
 
     @Id
