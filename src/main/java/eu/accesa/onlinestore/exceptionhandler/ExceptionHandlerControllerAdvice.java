d44974f00fe836ce10e4ad2721e89fec7c990be6
ACCESA\madalin.sava <savamadalinmihai@gmail.com>
2021-02-25 11:23:17 +0200

    added controller advice class

---
+++ src/main/java/eu/accesa/onlinestore/exceptionhandler/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,56 @@
+package eu.accesa.onlinestore.exceptionhandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+
+    private final static Logger logger = LoggerFactory.getLogger(ExceptionHandlerControllerAdvice.class);
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleEntityNotFoundException(EntityNotFoundException exception) {
+        logger.warn(exception.getMessage());
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(OnlineStoreException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleLearningPlatformException(OnlineStoreException onlineStoreException) {
+        return onlineStoreException.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception exception) {
+        logger.warn(exception.getMessage(), exception);
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<ObjectError> allErrors = exception.getBindingResult().getAllErrors();
+        List<String> body = new ArrayList<>();
+
+        for (ObjectError object : allErrors) {
+            body.add(object.getDefaultMessage());
+        }
+        return body;
+    }
+}
