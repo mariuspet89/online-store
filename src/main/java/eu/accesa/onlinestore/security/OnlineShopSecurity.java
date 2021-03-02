04d51e635caf4b95493d2f18463e988f896bc333
marius.petrut <marius.petrut@accesa.eu>
2021-03-02 13:16:24 +0200

    initial setup

---
+++ src/main/java/eu/accesa/onlinestore/security/OnlineShopSecurity.java
@@ -0,0 +1,22 @@
+package eu.accesa.onlinestore.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+@Configuration
+public class OnlineShopSecurity extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+}
