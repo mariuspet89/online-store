d014bc4898ae473ca87e311ad4a04ed84b0ad722
Marius Petrut <marius.petrut@accesa.eu>
2021-03-04 14:31:17 +0000

    Revert "Merge branch 'BCrypt_feature' into 'develop'"
    This reverts merge request !15

--- src/main/java/eu/accesa/onlinestore/repository/UserRepository.java
+++ src/main/java/eu/accesa/onlinestore/repository/UserRepository.java
@@ -3,9 +3,5 @@ package eu.accesa.onlinestore.repository;
 import eu.accesa.onlinestore.model.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends MongoRepository<UserEntity, String> {
-
-    Optional<UserEntity> findByUsername(String username);
 }
