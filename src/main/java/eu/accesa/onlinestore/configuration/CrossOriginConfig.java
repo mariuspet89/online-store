0e457efc80502c804f282f714391163dffe8cbcb
Dan Goia <dan.goia@accesa.eu>
2021-03-05 22:09:23 +0200

    implemented WebMvcConfigurer to CrossOringinConfig class

--- src/main/java/eu/accesa/onlinestore/configuration/CrossOriginConfig.java
+++ src/main/java/eu/accesa/onlinestore/configuration/CrossOriginConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CrossOriginConfig {
+public class CrossOriginConfig  implements WebMvcConfigurer{
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
