40b8d825e8a0fa445e3b0a0da5ddcace40fce9d1
ACCESA\doru.varga <ACCESA\doru.varga@accesa.eu>
2021-03-09 16:39:55 +0200

    name change product service test class

--- src/test/java/eu/accesa/onlinestore/service/implementation/ProductServiceImplTest.java
+++ src/test/java/eu/accesa/onlinestore/service/implementation/ProductServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProgramServiceImplTest {
+public class ProductServiceImplTest {
 
     @Mock
     private ProductRepository productRepository;
