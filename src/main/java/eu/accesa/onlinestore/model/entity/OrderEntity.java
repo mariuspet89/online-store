0979faa524c304362da0027d1de0af3449ae056e
Dan Goia <dan.goia@accesa.eu>
2021-03-09 15:53:38 +0200

    added findAll tests for Order

--- src/main/java/eu/accesa/onlinestore/model/entity/OrderEntity.java
+++ src/main/java/eu/accesa/onlinestore/model/entity/OrderEntity.java
@@ -1,20 +1,30 @@
 package eu.accesa.onlinestore.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import nonapi.io.github.classgraph.json.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import javax.validation.constraints.Past;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Objects;
 
 @Document(collection = "orders")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderEntity {
     @Id
     private String id;
     @Field(value = "ordered_products")
     private HashMap<String, Integer> orderedProducts;
     @Field(value = "order_date")
+    @JsonDeserialize(using= LocalDateTimeDeserializer.class)
+    @JsonSerialize(using= LocalDateTimeSerializer.class)
     private LocalDateTime orderDate;
     @Field(value = "order_value")
     private Double orderValue;
