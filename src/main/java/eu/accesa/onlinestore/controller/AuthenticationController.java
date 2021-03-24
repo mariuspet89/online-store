45f8d00554f8bcca46d2732224a14b51a7f9138d
Doru Varga <doru.varga@accesa.eu>
2021-03-24 09:48:52 +0000

    Email verification

--- src/main/java/eu/accesa/onlinestore/controller/AuthenticationController.java
+++ src/main/java/eu/accesa/onlinestore/controller/AuthenticationController.java
@@ -4,17 +4,17 @@ import eu.accesa.onlinestore.model.dto.AuthRequestDto;
 import eu.accesa.onlinestore.model.dto.UserDto;
 import eu.accesa.onlinestore.model.entity.UserEntity;
 import eu.accesa.onlinestore.configuration.security.JwtTokenUtil;
+import eu.accesa.onlinestore.service.implementation.UserServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,16 +24,18 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
     private final ModelMapper modelMapper;
+    private final UserServiceImpl userService;
 
     public AuthenticationController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil,
-                                    ModelMapper modelMapper) {
+                                    ModelMapper modelMapper, UserServiceImpl userService) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenUtil = jwtTokenUtil;
         this.modelMapper = modelMapper;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDto> login(@RequestBody @Valid AuthRequestDto request) {
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthRequestDto request) {
         try {
             Authentication authentication = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(
@@ -46,7 +48,16 @@ public class AuthenticationController {
                     .header(HttpHeaders.AUTHORIZATION, jwtTokenUtil.generateAccessToken(userEntity))
                     .body(modelMapper.map(userEntity, UserDto.class));
         } catch (BadCredentialsException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Username or password is wrong!");
+        } catch (DisabledException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Please verify your account first!");
         }
+    }
+
+    @PutMapping("/userConfirmation")
+    public ResponseEntity<String> method(@RequestParam("userId") String userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.confirmUser(userId));
     }
 }
--- src/main/java/eu/accesa/onlinestore/service/EmailService.java
+++ src/main/java/eu/accesa/onlinestore/service/EmailService.java
@@ -6,22 +6,4 @@ public interface EmailService {
     void sendSimpleMessage(String to,
                            String subject,
                            String text);
-//    void sendSimpleMessageUsingTemplate(String to,
-//                                        String subject,
-//                                        String ...templateModel);
-//    void sendMessageWithAttachment(String to,
-//                                   String subject,
-//                                   String text,
-//                                   String pathToAttachment);
-//
-//    void sendMessageUsingThymeleafTemplate(String to,
-//                                           String subject,
-//                                           Map<String, Object> templateModel)
-//            throws IOException, MessagingException;
-//
-//    void sendMessageUsingFreemarkerTemplate(String to,
-//                                            String subject,
-//                                            Map<String, Object> templateModel)
-//            throws IOException, TemplateException, MessagingException;
-
 }
