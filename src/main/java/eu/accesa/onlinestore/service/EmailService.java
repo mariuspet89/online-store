b815e197ff9fc63ff38433a0d52ec13ff12d2f6d
ACCESA\doru.varga <ACCESA\doru.varga@accesa.eu>
2021-03-25 13:35:10 +0200

    da

--- src/main/java/eu/accesa/onlinestore/service/EmailService.java
+++ src/main/java/eu/accesa/onlinestore/service/EmailService.java
@@ -6,4 +6,10 @@ public interface EmailService {
     void sendSimpleMessage(String to,
                            String subject,
                            String text);
+
+
+    void sendMessageWithAttachment(String to,
+                                   String subject,
+                                   String text,
+                                   String pathToAttachment);
 }
--- src/main/java/eu/accesa/onlinestore/service/implementation/EmailServiceImpl.java
+++ src/main/java/eu/accesa/onlinestore/service/implementation/EmailServiceImpl.java
@@ -2,10 +2,16 @@ package eu.accesa.onlinestore.service.implementation;
 
 import eu.accesa.onlinestore.service.EmailService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -35,4 +41,28 @@ public class EmailServiceImpl implements EmailService {
         }
     }
 
+
+    @Override
+    public void sendMessageWithAttachment(String to,
+                                          String subject,
+                                          String text,
+                                          String pathToAttachment) {
+        try {
+            MimeMessage message = emailSender.createMimeMessage();
+            // pass 'true' to the constructor to create a multipart message
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(NOREPLY_ADDRESS);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+
+            FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+            helper.addAttachment("Invoice", file);
+
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
