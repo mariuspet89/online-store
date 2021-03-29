24661cf72169b50e043d4bbb9135c6ab09a5a9df
Robert Revnic <robert.revnic@accesa.eu>
2021-03-29 16:33:04 +0300

    Send email via Thymeleaf template

--- src/main/java/eu/accesa/onlinestore/service/EmailService.java
+++ src/main/java/eu/accesa/onlinestore/service/EmailService.java
@@ -1,9 +1,17 @@
 package eu.accesa.onlinestore.service;
 
 
+import javax.mail.MessagingException;
+import java.util.Map;
+
 public interface EmailService {
 
     void sendSimpleMessage(String to,
                            String subject,
                            String text);
+
+    void sendMessageUsingThymeleafTemplate(String to,
+                                           String subject,
+                                           String template,
+                                           Map<String, Object> templateModel) throws MessagingException;
 }
--- src/main/java/eu/accesa/onlinestore/service/implementation/EmailServiceImpl.java
+++ src/main/java/eu/accesa/onlinestore/service/implementation/EmailServiceImpl.java
@@ -3,23 +3,32 @@ package eu.accesa.onlinestore.service.implementation;
 import eu.accesa.onlinestore.service.EmailService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private static final String NOREPLY_ADDRESS = "onlinestoreaccesa@gmail.com";
 
+    private final JavaMailSender emailSender;
+    private final SpringTemplateEngine thymeleafTemplateEngine;
 
-    private final MailSender emailSender;
-
-    public EmailServiceImpl(@Qualifier("emailSender") MailSender emailSender) {
+    public EmailServiceImpl(@Qualifier("emailSender") JavaMailSender emailSender, SpringTemplateEngine thymeleafTemplateEngine) {
         this.emailSender = emailSender;
+        this.thymeleafTemplateEngine = thymeleafTemplateEngine;
     }
 
-
+    @Override
     public void sendSimpleMessage(String to, String subject, String text) {
 
         try {
@@ -35,4 +44,23 @@ public class EmailServiceImpl implements EmailService {
         }
     }
 
+    @Override
+    public void sendMessageUsingThymeleafTemplate(String to, String subject, String template, Map<String, Object> templateModel) throws MessagingException {
+        Context context = new Context();
+        context.setVariables(templateModel);
+
+        String htmlBody = thymeleafTemplateEngine.process(template, context);
+
+        sendHtmlMessage(to, subject, htmlBody);
+    }
+
+    private void sendHtmlMessage(String to, String subject, String htmlBody) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+        helper.setFrom(NOREPLY_ADDRESS);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        emailSender.send(message);
+    }
 }
