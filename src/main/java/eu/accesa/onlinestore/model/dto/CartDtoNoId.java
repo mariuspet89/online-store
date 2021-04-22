bb99b92d2ce2781bfc553fca3a3f9446ce3659aa
Robert Revnic <robert.revnic@accesa.eu>
2021-04-22 14:28:03 +0300

    Added custom annotation for ordered product quantities

--- src/main/java/eu/accesa/onlinestore/model/dto/CartDtoNoId.java
+++ src/main/java/eu/accesa/onlinestore/model/dto/CartDtoNoId.java
@@ -1,10 +1,19 @@
 package eu.accesa.onlinestore.model.dto;
 
+import eu.accesa.onlinestore.model.annotation.OrderedQuantity;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 public class CartDtoNoId {
 
+    @NotBlank(message = "The user ID must not be null or empty!")
     private String userId;
+
+    @NotEmpty(message = "The ordered products list must not be null or empty!")
+    @OrderedQuantity
     private Map<String, Integer> products;
 
     public String getUserId() {
