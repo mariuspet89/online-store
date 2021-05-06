bf6fb421fb3fa03632ec5169dcc3595c708426c3
Robert Revnic <robert.revnic@accesa.eu>
2021-05-06 12:08:27 +0300

    Added mail properties in both Java config class and app.properties

--- src/main/java/eu/accesa/onlinestore/configuration/JavaMailSenderConfig.java
+++ src/main/java/eu/accesa/onlinestore/configuration/JavaMailSenderConfig.java
@@ -25,7 +25,7 @@ public class JavaMailSenderConfig {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "*");
 
         return mailSender;
     }
